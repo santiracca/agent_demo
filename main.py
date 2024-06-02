88789bd654e0687f97efff3be909c7a44a36a004
Santi Racca <santiagoracca7@gmail.com>
2024-06-02 16:30:52 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.experimental.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+
+load_dotenv()
+
+population_path = os.path.join("data", "population.csv")
+population_df = pd.read_csv(population_path)
+
+population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+
+
+
+tools = [
+  note_engine,
+  QueryEngineTool(
+    query_engine=population_query_engine,
+    metadata=ToolMetadata(
+     name="population_data",
+     description="This gives information about the world population and demographics",
+  ))
+]
+
+
+llm = OpenAI(model="gpt-4")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+  result = agent.query(prompt)
+  print(result)
+
+
+
+
+
